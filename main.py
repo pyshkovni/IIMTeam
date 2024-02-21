c8e8157309676607d8b488b219d47081fa313b8c
pyshkovni <85805562+pyshkovni@users.noreply.github.com>
2024-02-21 09:05:45 +0000

    Добавил новый хендлер newbot

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ bot = telebot.TeleBot("7020192176:AAG9G16SSQDgXMI8_3674vIVc4WXgi01UM8")
 def start(message):
     bot.reply_to(message, "Привет!")
 
+
+@bot.message_handler(commands=["newmsg"])
+def newbot(message):
+    bot.reply_to(message, "Новое сообщение!")
+
+
 bot.polling()
 
